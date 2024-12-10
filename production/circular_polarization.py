a3eda67ba6980bebeebda69893a2b73da9b30b7e
TFL4N <tflan10@gmail.com>
2024-12-10 15:34:33 -0500

    update circle polars.py

--- production/circular_polarization.py
+++ production/circular_polarization.py
@@ -1,24 +1,33 @@
 from manim import *
 import numpy as np
 
+# scene types
+NO_COMPS = 0
+VERT_ONLY = 1
+HORZ_ONLY = 2
+SUPER_POS = 3
 
-
-
-class EllipticPolarization(ThreeDScene):
+class CircularPolarization(ThreeDScene):
     def construct(self):
-        # time range
-        t_start = 0
-        t_end = 10*PI
-
+        scene_type = VERT_ONLY
+        
+        # time range of propagation segment
+        t_range = 10*PI
+        t_init = 0
+        t_final = t_range
+        
+        # animate time value
+        t_end = ValueTracker(t_init)
+        
         # build electric field coordinates
         phase = PI/4
         def total_e(t):
             return np.array([t, np.cos(t), np.cos(t+phase)])
-    
+
     
         # 3D coordinate system
         axes = ThreeDAxes(
-            x_range=[t_start,t_end,PI/4],
+            x_range=[0,t_range,PI/4],
             y_range=[-2,2,1],
             z_range=[-2,2,1],
             x_length=15,
@@ -26,46 +35,68 @@ class EllipticPolarization(ThreeDScene):
             z_length=10
         )
 
-        # animate time value
-        t_val = ValueTracker(t_start)
-
-        # electric field vs time
+        # propagating electric field vs time
         graph = always_redraw(lambda :
                               axes.plot_parametric_curve(total_e,
-                                                         t_range=[t_start,t_val.get_value(),0.5],
+                                                         t_range=[t_end.get_value()-t_range, t_end.get_value(), 0.5],
                                                          color=RED)
                               )
 
-        # electric field projected into yz plane
-        def proj_e(x, t):
-            e = total_e(t)
-            e[0] = x
-            return e
-        circle = always_redraw(lambda :
-                               axes.plot_parametric_curve(lambda t: proj_e(t_val.get_value(), t),
-                                                          t_range=[0,2*PI,0.5],
-                                                          color=RED
-                                                          )
-                               )
+        self.add(axes, graph)
 
-        # electric field vector in projection
-        def radial_arrow(t):
-            return Arrow3D(
-                start=axes.c2p(t, 0, 0),
-                end=axes.c2p(*total_e(t)),
-                resolution=8,
-                color=BLUE
-            )
+        #
+        # Objects per scene type
+        #
+        if scene_type == VERT_ONLY:
+            # vertical component of electric field
+            def comp_vector(t):
+                padding = 1.5
+                e = total_e(t)
+                e[1] = padding
+
+                return Arrow3D(
+                    start=axes.c2p(t, padding, 0),
+                    end=axes.c2p(*e),
+                    resolution=8,
+                    color=BLUE
+                )
+
+            comp = always_redraw(lambda : comp_vector(t_end.get_value()))
+            self.add(comp)
+            
+        elif scene_type == SUPER_POS:
+            # electric field y & z components translated by x
+            def proj_e(x, t):
+                e = total_e(t)
+                e[0] = x
+                return e
+
+            circle = always_redraw(lambda :
+                                   axes.plot_parametric_curve(lambda t: proj_e(t_end.get_value(), t),
+                                                              t_range=[0,2*PI,0.5],
+                                                              color=RED
+                                                              )
+                                   )
+            
+            # electric field vector in projection
+            def radial_arrow(t):
+                return Arrow3D(
+                    start=axes.c2p(t, 0, 0),
+                    end=axes.c2p(*total_e(t)),
+                    resolution=8,
+                    color=BLUE
+                )
                 
-
-        radial = always_redraw(lambda : radial_arrow(t_val.get_value()))
-
+            radial = always_redraw(lambda : radial_arrow(t_end.get_value()))
+            self.add(circle, radial)
+        
+            
         # electric field vector checkpoints
         lines = VGroup()
         dx_line = 0.5
         
         def update_lines(group, alpha):
-            line_pos = int(alpha * (t_end-t_start) / dx_line)
+            line_pos = int(alpha * t_range / dx_line)
             if line_pos > len(group.submobjects):
                 new_line = Arrow3D(
                     start=axes.c2p(line_pos*dx_line, 0, 0),
@@ -75,9 +106,12 @@ class EllipticPolarization(ThreeDScene):
                 )
                 lines.add(new_line)
                 
-        # add objects to scene
-        self.add(axes, graph, lines, circle, radial)
-                
+        self.add(lines)
+        
+
+        #
+        # Begin animations
+        #
     
         # init camera position
         self.set_camera_orientation(phi=75 * DEGREES,
@@ -86,7 +120,7 @@ class EllipticPolarization(ThreeDScene):
         #self.begin_ambient_camera_rotation(rate=0.1)
     
         # animate
-        self.play(t_val.animate.set_value(t_end),
+        self.play(t_end.animate.set_value(t_final),
                   UpdateFromAlphaFunc(graph, update_lines),
                   run_time=5,
                   rate_func=linear)
